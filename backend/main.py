411932c1ad387f562c81577d89a046f58bdbce63
Hrectifed <133038949+Hrectifed@users.noreply.github.com>
2025-07-21 01:34:17 +0200

    Update CORS settings and add SSL certificate support for local development

--- backend/main.py
+++ backend/main.py
@@ -9,9 +9,19 @@ import re
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://127.0.0.1:3000",
+        "http://127.0.0.1:3000",
+        "https://localhost:3000",
+        "http://localhost:3000",
+        
+        "https://127.0.0.1:8000",
+        "http://127.0.0.1:8000",
+        "https://localhost:8000",
+        "http://localhost:8000",
+    ],
     allow_credentials=True,
-    allow_methods=["*"] ,
+    allow_methods=["*"],
     allow_headers=["*"]
 )
 
